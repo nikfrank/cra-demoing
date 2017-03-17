cffa4e27cabd27af03ec77f20d8924d8c2800346
nik frank <nikbfrank@gmail.com>
2017-03-17 13:30:00 +0200

    made a fancy button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import FancyButton from './FancyButton';
+
 class App extends Component {
   render() {
     return (
@@ -10,11 +12,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        { [1,2,3,4,10].map( i => (
+        {
+          [1,2,3,4,10].map( i => (
             <p className="App-intro">
               {i*i*i} To get started, edit <code>src/App.js</code> and save to reload.
             </p>
-        ) ) }
+          ) )
+        }
+        <FancyButton />    
       </div>
     );
   }
---
+++ src/FancyButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FancyButton extends Component {
+  render() {
+    return (
+      <div>
+        <p>You should</p>
+        <button>Click Me!</button>
+      </div>
+    );
+  }
+}
+
+export default FancyButton;
