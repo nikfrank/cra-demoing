2909cbfe02ceaea98245ce58337641ae2aff8433
nik frank <nikbfrank@gmail.com>
2017-03-17 12:49:41 +0200

    lopped a p

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { [1,2,3,4,10].map( i => (
+            <p className="App-intro">
+              {i*i*i} To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        ) ) }
       </div>
     );
   }
